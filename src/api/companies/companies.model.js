0781ba0a95e8814334a78e02a144ddc3227e8dec
mistymidnightss <mistydeveloping@gmail.com>
2022-08-05 17:02:08 +0200

    changes and save vercel

--- src/api/companies/companies.model.js
+++ src/api/companies/companies.model.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema ({
-    name: { type: String, unique: true, requiered: true },
-    year: {type: Number, unique: true, required: true},
-    country: {type: String, unique: true, required: true},
+const schema = new Schema({
+  name: { type: String, unique: true, requiered: true },
+  year: { type: Number, unique: true, required: true },
+  country: { type: String, unique: true, required: true },
 });
 
-module.exports = mongoose.model('companies', schema);
+module.exports = mongoose.model("companies", schema);
